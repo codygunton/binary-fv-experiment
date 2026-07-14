5df092f70ecc43a2fabe83191e8ec2ea66ac8afa
Cody Gunton <codygunton@gmail.com>
2026-07-14 06:06:18 -0400

    Prove generated fetch memory path

--- BinaryFv.lean
+++ BinaryFv.lean
@@ -7,6 +7,7 @@ import BinaryFv.RISCV.FetchContract
 import BinaryFv.RISCV.PmpContract
 import BinaryFv.RISCV.TranslationContract
 import BinaryFv.RISCV.PhysicalAccessContract
+import BinaryFv.RISCV.FetchMemoryContract
 import BinaryFv.RISCV.PostludePrimitives
 import BinaryFv.RISCV.StackStepContract
 import BinaryFv.RISCV.TryStepStackAddiContract
