af2ee2e182e8a1d6476a1c829c26b70ad3b030be
Cody Gunton <codygunton@gmail.com>
2026-07-14 05:11:45 -0400

    Frame generated M-extension writes

--- BinaryFv.lean
+++ BinaryFv.lean
@@ -14,6 +14,7 @@ import BinaryFv.RISCV.RegisterFrame
 import BinaryFv.RISCV.RTypeFrame
 import BinaryFv.RISCV.ITypeFrame
 import BinaryFv.RISCV.ShiftIopFrame
+import BinaryFv.RISCV.MulDivFrame
 import BinaryFv.RISCV.InstructionContracts
 import BinaryFv.RISCV.ExecuteContract
 import BinaryFv.Keccak.Root
