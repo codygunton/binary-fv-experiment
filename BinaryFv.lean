1450d61127db8871047e8d90e07b3c1408a6b76a
Cody Gunton <codygunton@gmail.com>
2026-07-14 06:09:07 -0400

    Lift generated integer frames through execute

--- BinaryFv.lean
+++ BinaryFv.lean
@@ -25,6 +25,7 @@ import BinaryFv.RISCV.ITypeFrame
 import BinaryFv.RISCV.ShiftIopFrame
 import BinaryFv.RISCV.MulDivFrame
 import BinaryFv.RISCV.FenceFrame
+import BinaryFv.RISCV.IntegerDispatchFrame
 import BinaryFv.RISCV.ReadFrame
 import BinaryFv.RISCV.DecodeFrame
 import BinaryFv.RISCV.EnabledFrame
