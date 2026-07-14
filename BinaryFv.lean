64bcf4bb14e8446e6ad97b97406e3cd0f8d9cae0
Cody Gunton <codygunton@gmail.com>
2026-07-14 05:19:24 -0400

    Frame generated fence execution

--- BinaryFv.lean
+++ BinaryFv.lean
@@ -32,6 +32,7 @@ import BinaryFv.RISCV.RTypeFrame
 import BinaryFv.RISCV.ITypeFrame
 import BinaryFv.RISCV.ShiftIopFrame
 import BinaryFv.RISCV.MulDivFrame
+import BinaryFv.RISCV.FenceFrame
 import BinaryFv.RISCV.InstructionContracts
 import BinaryFv.RISCV.ExecuteContract
 import BinaryFv.Keccak.Root
