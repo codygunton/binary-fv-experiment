9095e2ba3c438bf7245c8ad7e4f9b903111fde6f
Cody Gunton <codygunton@gmail.com>
2026-07-18 02:15:59 -0400

    Model Zesu bump allocator bounds

--- BinaryFv/SSZ.lean
+++ BinaryFv/SSZ.lean
@@ -8,6 +8,7 @@ import BinaryFv.SSZ.Zesu.Analysis.FunctionWords
 import BinaryFv.SSZ.Zesu.Analysis.Reachability
 import BinaryFv.SSZ.Zesu.Analysis.AllocatorCalls
 import BinaryFv.SSZ.Zesu.Execution.Representation
+import BinaryFv.SSZ.Zesu.Proof.Runtime.BumpAllocator
 import BinaryFv.SSZ.Zesu.Interface
 import BinaryFv.SSZ.Root
 
