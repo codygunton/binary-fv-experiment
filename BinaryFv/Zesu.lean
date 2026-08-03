3e2b007ac3966afe51bd1a82912306a0ccaef3d4
Cody Gunton <codygunton@gmail.com>
2026-08-03 19:49:46 -0400

    Integrate Level 2 outgoing branch proof

--- BinaryFv/Zesu.lean
+++ BinaryFv/Zesu.lean
@@ -23,6 +23,7 @@ import BinaryFv.Zesu.MachineExecution.ParserBlocks
 import BinaryFv.Zesu.MachineExecution.AccessorBlocks
 import BinaryFv.Zesu.MachineExecution.Level2AllocatorProof
 import BinaryFv.Zesu.MachineExecution.Level2WrapperProof
+import BinaryFv.Zesu.MachineExecution.Level2OutgoingBranchSteps
 import BinaryFv.Zesu.MachineExecution.MemcpyProof
 import BinaryFv.Zesu.MachineExecution.HasExactErePrefixProof
 import BinaryFv.Zesu.MachineExecution.DecodeInlineProof
