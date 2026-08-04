89db1b5fe7e2a90973c746f415aad5e20ed5725a
Cody Gunton <codygunton@gmail.com>
2026-08-03 20:06:51 -0400

    Define Level 4 contract surface

--- BinaryFv/Zesu.lean
+++ BinaryFv/Zesu.lean
@@ -31,6 +31,7 @@ import BinaryFv.Zesu.Entrypoints.ZesuDecodeRaw.Level3Boundaries
 import BinaryFv.Zesu.Entrypoints.ZesuDecodeRaw.Level2Contracts
 import BinaryFv.Zesu.Entrypoints.ZesuDecodeRaw.Level3Contracts
 import BinaryFv.Zesu.Entrypoints.ZesuDecodeRaw.Level4BoundaryInventory
+import BinaryFv.Zesu.Entrypoints.ZesuDecodeRaw.Level4ContractSurface
 import BinaryFv.Zesu.MemoryRepresentation.RawV4
 import BinaryFv.Zesu.MemoryRepresentation.Observers
 import BinaryFv.Zesu.MemoryRepresentation.Result
